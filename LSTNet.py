3f6346752f16afb862a7a1c226bb2b05b029e53f
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-09-27 13:06:15 -0400

    added boilerplate
    added boilerplate

---
+++ LSTNet.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+class LSTNet(tf.keras.layers.Layer):
+
+    def __init__(self):
+        super().__init__()
+
+    def build():
+        pass
+
+    def call():
+        pass
