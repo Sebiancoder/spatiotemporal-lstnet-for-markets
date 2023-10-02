27832c346a35b854e9052d4247d2742e6b422ad4
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-10-02 16:43:30 -0400

    Add data
    add data

--- LSTNet.py
+++ LSTNet.py
@@ -2,8 +2,13 @@ import tensorflow as tf
 
 class LSTNet(tf.keras.layers.Layer):
 
-    def __init__(self):
+    def __init__(self, num_heads = 5, key_dim = 5):
+
         super().__init__()
+
+        temp_self_att = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=key_dim)
+
+        spat_self_att = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=key_dim)    
 
     def build():
         pass
