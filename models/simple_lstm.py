6c1d7c4db7d8d0546acca16109224ba20892fd7c
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-10-16 17:30:07 -0400

    tried to make lstm work
    tried to make lstm work

--- models/simple_lstm.py
+++ models/simple_lstm.py
@@ -6,7 +6,8 @@ class SimpleLSTM(tf.keras.Model):
 
         super().__init__()
 
-        self.denseLayer = tf.keras.layers.Dense(16, activation='sigmoid')  
+        self.denseLayer = tf.keras.layers.Dense(64, activation='sigmoid')
+        self.outputDense = tf.keras.layers.Dense(1, activation='sigmoid')  
 
     def build(self, inputs):
         
@@ -18,10 +19,18 @@ class SimpleLSTM(tf.keras.Model):
             return_sequences=False, 
             stateful=False)
 
+        self.hidden_lstm = tf.keras.layers.LSTM(
+            units=64, 
+            activation="tanh", 
+            recurrent_activation="sigmoid", 
+            recurrent_dropout=0.2, 
+            return_sequences=True, 
+            stateful=False)
+
     def call(self, inputs):
         
-        x = self.lstm_layer(inputs)
+        x = self.lstm_layer(self.hidden_lstm(inputs))
 
         x = self.denseLayer(x)
 
-        return x
+        return self.outputDense(x)
