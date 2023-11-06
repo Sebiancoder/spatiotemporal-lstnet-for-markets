55216abeddd77a1216314a89509030c8255cba3e
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-11-06 11:45:46 -0500

    fixed testing loop code
    fixed testing loop code, previously code was not properly preprocessing testing data

--- models/mlp.py
+++ models/mlp.py
@@ -10,12 +10,16 @@ class MLP(tf.keras.Model):
         self.hparams = hparams
 
         self.dense_layers = []
+        self.do_layers = []
 
-        for i in range(self.hparams.int("d1", default=3, min_value=2, max_value=5, step=1)):
+        for i in range(self.hparams.Int("num_layers", default=3, min_value=2, max_value=5, step=1)):
 
-            self.dense_layers.append(tf.keras.layers.Dense(self.hparams.int("d_{I}".format(I=i), default=64, min_value=16, max_value=512, step=16), activation="relu"))
+            self.dense_layers.append(tf.keras.layers.Dense(self.hparams.Int("d_{I}".format(I=i), default=64, min_value=16, max_value=512, step=16), activation="relu"))
+            self.do_layers.append(tf.keras.layers.Dropout(self.hparams.Float("dropout", default=0.1, min_value=0.05, max_value=0.3, step=0.05)))
 
         self.outputDense = tf.keras.layers.Dense(1, activation="relu")
+
+        self.flattener = tf.keras.layers.Flatten()
 
     def build(self, input_shape):
 
@@ -23,10 +27,11 @@ class MLP(tf.keras.Model):
 
     def call(self, inputs):
         
-        x = inputs
+        x = self.flattener(inputs)
         
-        for layer in self.dense_layers:
+        for layer_index in range(len(self.dense_layers)):
 
-            x = layer(x)
+            x = self.dense_layers[layer_index](x)
+            x = self.do_layers[layer_index](x)
 
         return self.outputDense(x)
