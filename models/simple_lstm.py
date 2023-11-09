9084d8a4829fbc63a78ff40a3f879b0a4aef3c7a
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-11-08 23:16:52 -0500

    updated lstm for variable number of layers
    updated lstm for variable number of layers

--- models/simple_lstm.py
+++ models/simple_lstm.py
@@ -11,27 +11,35 @@ class SimpleLSTM(tf.keras.Model):
         self.outputDense = tf.keras.layers.Dense(1, activation='relu')  
 
     def build(self, input_shape):
-        
-        self.lstm_layer = tf.keras.layers.LSTM(
-            units=self.hparams.Int("lstm_units1", default=128, min_value=16, max_value=256, step=16), 
-            activation="tanh", 
-            recurrent_activation="sigmoid", 
-            recurrent_dropout=self.hparams.Float("dropout", default=0.1, min_value=0.05, max_value=0.3, step=0.05), 
-            return_sequences=False, 
-            stateful=False)
 
-        self.hidden_lstm = tf.keras.layers.LSTM(
-            units=self.hparams.Int("lstm_units2", default=128, min_value=16, max_value=256, step=16), 
-            activation="tanh", 
-            recurrent_activation="sigmoid", 
-            recurrent_dropout=self.hparams.Float("dropout", default=0.1, min_value=0.05, max_value=0.3, step=0.05), 
-            return_sequences=True, 
-            stateful=False)
+        self.hidden_lstms = []
+
+        for i in range(self.hparams.Int("num_lstsms", default=2, min_value=1, max_value=5, step=1)):
+        
+            self.hidden_lstms.append(tf.keras.layers.LSTM(
+                units=self.hparams.Int("lstm_units_{I}".format(I=i), default=128, min_value=16, max_value=256, step=16), 
+                activation="tanh", 
+                recurrent_activation="sigmoid", 
+                recurrent_dropout=self.hparams.Float("dropout", default=0.1, min_value=0.05, max_value=0.3, step=0.05), 
+                return_sequences=True, 
+                stateful=False))
+
+        self.outputLSTM = tf.keras.layers.LSTM(
+                units=self.hparams.Int("output_lstm", default=128, min_value=16, max_value=256, step=16), 
+                activation="tanh", 
+                recurrent_activation="sigmoid", 
+                recurrent_dropout=self.hparams.Float("dropout", default=0.1, min_value=0.05, max_value=0.3, step=0.05), 
+                return_sequences=False, 
+                stateful=False)
 
         super().build(input_shape)
 
     def call(self, inputs):
         
-        x = self.lstm_layer(self.hidden_lstm(inputs))
+        x = inputs
+        
+        for lstm in self.hidden_lstms:
 
-        return self.outputDense(x)
+            x = lstm(x)
+
+        return self.outputDense(self.outputLSTM(x))
