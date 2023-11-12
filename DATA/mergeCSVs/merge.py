43765421a2abf06efb78a592b660217c73910753
dylan kelly <dkelly66@gatech.edu>
2023-11-12 15:54:21 -0500

    Merged Aidan's data into output dataset

--- DATA/mergeCSVs/merge.py
+++ DATA/mergeCSVs/merge.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 # Load each CSV file into DataFrames
-df1 = pd.read_csv('Rapeseed Futures Historical Data.csv')
-df2 = pd.read_csv('US Corn Futures Historical Data.csv')
-df3 = pd.read_csv('US Soybeans Futures Historical Data.csv')
-df4 = pd.read_csv('US Wheat Futures Historical Data.csv')
-df5 = pd.read_csv('Castorseed Futures Historical Data.csv')
+df1 = pd.read_csv('DATA/mergeCSVs/RapeseedFuturesHistoricalData.csv')
+df2 = pd.read_csv('DATA/mergeCSVs/USCornFuturesHistoricalData.csv')
+df3 = pd.read_csv('DATA/mergeCSVs/USSoybeansFuturesHistoricalData.csv')
+df4 = pd.read_csv('DATA/mergeCSVs/USWheatFuturesHistoricalData.csv')
+df5 = pd.read_csv('DATA/mergeCSVs/CastorseedFuturesHistoricalData.csv')
+df6 = pd.read_csv('DATA/mergeCSVs/ADMHistoricalData.csv')
+df7 = pd.read_csv('DATA/mergeCSVs/BGHistoricalData.csv')
+df8 = pd.read_csv('DATA/mergeCSVs/DDHistoricalData.csv')
 
 # Merge the DataFrames on the "Data" column
 # merged_df = df1.merge(df2, on='Date', how='inner')
@@ -15,8 +18,16 @@ merged_df = pd.merge(merged_df,df3[['Date','Price']],on='Date', how='left')
 merged_df = merged_df.rename(columns={"Price_x": "Rapeseed", "Price_y": "Corn", "Price": "Soybeans"})
 merged_df = pd.merge(merged_df,df4[['Date','Price']],on='Date', how='left')
 merged_df = pd.merge(merged_df,df5[['Date','Price']],on='Date', how='left')
-merged_df = merged_df.rename(columns={"Price_x": "What", "Price_y": "Castorseed"})
+merged_df = merged_df.rename(columns={"Price_x": "Wheat", "Price_y": "Castorseed"})
 merged_df = merged_df.dropna()
+merged_df = pd.merge(merged_df,df6[['Date','Price']],on='Date', how='left')
+merged_df = pd.merge(merged_df, df7[['Date','Price']],on='Date', how='left')
+merged_df = merged_df.rename(columns={"Price_x": "ADM", "Price_y": "BG"})
+merged_df = pd.merge(merged_df, df8[['Date','Price']],on='Date', how='left')
+merged_df = merged_df.rename(columns={"Price": "DD"})
+
+
+
 merged_df = merged_df.replace(',','', regex=True)
 merged_df.astype({col: float for col in merged_df.columns[1:]})
 
@@ -33,4 +44,4 @@ merged_df.astype({col: float for col in merged_df.columns[1:]})
 # price_columns = ['Price_x', 'Price_y', 'Price_x_x', 'Price_y_x', 'Price']
 
 # The merged DataFrame now contains the "Price" data from all five CSV files, joined on the "Data" column.
-merged_df.to_csv('output.csv', index=False)
+merged_df.to_csv('DATA/mergeCSVs/output.csv', index=False)
